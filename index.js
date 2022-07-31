6601a6831538ba0af4ae843ff7591acc1358f3f0
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-07-31 20:48:25 +0600

    generate jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -142,7 +143,10 @@ async function run() {
                 $set: user,
             }
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.send({ result, token });
         })
 
         app.put('/users/admin/:email', async (req, res) => {
